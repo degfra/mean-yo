b9bde99a0411a455c38c7ba8bfb456247b696860
degfra <f.degrandi@stemcode.ch>
2015-04-27 18:43:13 +0200

    Added angular filters and ng-bind-html element to unescape html code

--- public/modules/events/controllers/events.client.controller.js
+++ public/modules/events/controllers/events.client.controller.js
@@ -3,5 +3,17 @@
 angular.module('events').controller('EventsController', ['$scope',
 	function($scope) {
 		$scope.title = 'High Performance WebSocket';
+		$scope.event = {
+			'name': '"Developing Offline Applications" and "HTML 5 Animations"',
+			'time': 1411430400000,
+			'event_url': 'http://www.meetup.com/Frontend-Meetup-Romandie/events/221571857/',
+			'description': '<p><b>6 pm : "Developing Offline Applications with HTML 5" by Venkat Subraniam</b></p>',
+			'venue': {
+				'name': 'Rally Software',
+				'address_1': '1550 Wynkoop',
+				'city': 'Lausanne',
+				'state': 'Vaud'
+			}
+		}
 	}
 ]);
